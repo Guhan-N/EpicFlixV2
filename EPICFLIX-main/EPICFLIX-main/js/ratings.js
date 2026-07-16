03487ae64c2c7d8ff6ef11e111cc0a35e4a1aae0
Guhan <guhan6575@gmail.com>
2026-07-16 18:09:05 +0530

    Delete EPICFLIX-main/EPICFLIX-main directory

--- EPICFLIX-main/EPICFLIX-main/js/ratings.js
+++
@@ -1,9 +0,0 @@
-import { StorageManager } from './storage.js';
-
-export function setRating(type, id, value) {
-  return StorageManager.rating.set(type, id, value);
-}
-
-export function getRating(type, id) {
-  return StorageManager.rating.get(type, id);
-}
--- EPICFLIX-main/EPICFLIX-main/js/search.js
+++
@@ -1,9 +0,0 @@
-import { StorageManager } from './storage.js';
-
-export function addSearchTerm(term) {
-  return StorageManager.searchHistory.add(term);
-}
-
-export function getSearchHistory() {
-  return StorageManager.searchHistory.get();
-}
